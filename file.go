54ab54ed50931d1b297e20cd1de47c311cb4c4f5
ccongdeng <dhclavie@outlook.com>
2019-07-30 12:14:43 +0800

    update format

--- file.go
+++ file.go
@@ -14,11 +14,11 @@ func IsFileExist(filePath string) (bool, error) {
 	info, err := os.Stat(filePath)
 	if err != nil {
 		if os.IsNotExist(err) {
-			return false,nil
+			return false, nil
 		}
 		return false, err
 	}
-	return !info.IsDir(),nil
+	return !info.IsDir(), nil
 }
 
 func Download(url string, filePath string) error {
@@ -53,4 +53,3 @@ func Delete(filePath string) error {
 func RandomTmpFilePath() string {
 	return TMP_DIR + "/" + NewUUID()
 }
-
